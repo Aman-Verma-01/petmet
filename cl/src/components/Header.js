cf954d10159925fe324aa750159c2763dc5b810c
Aman-Verma-01 <amanvcs01@gmail.com>
2024-02-01 00:02:07 +0530

    git add

---
+++ cl/src/components/Header.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import { Link, useLocation } from 'react-router-dom';
+
+function Header(props) {
+  const { sections, title } = props;
+  const location = useLocation();
+
+  return (
+    <React.Fragment>
+      <Toolbar sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Typography
+          component="h10"
+          variant="h5"
+          color="inherit"
+          align="center"
+          sx={{ flex: 2 }}
+        >
+          <Link to='/'>
+            <img src="images/logo.png" width='150' height='150'/>
+          </Link>
+        
+        </Typography>
+      </Toolbar>
+      <Toolbar
+        component="nav"
+        variant="dense"
+        sx={{ justifyContent: 'space-between', overflowX: 'auto' }}
+      >
+        {sections.map((section) => (
+          <Link
+            to={section.url}
+            key={section.url}
+            className={location.pathname === section.url ? 'activeNavLink' : ''}
+          >
+            {section.title}
+          </Link>
+        ))}
+      </Toolbar>
+    </React.Fragment>
+  );
+}
+
+Header.propTypes = {
+  sections: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default Header;
